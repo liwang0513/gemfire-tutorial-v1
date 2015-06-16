56c18dceded3ff9eaa342f35adac872606abd679
Li Wang <liwang@pivotal.io>
2015-06-16 18:21:33 -0400

    small changes

---
+++ PreLoader/preloader.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+java -cp $GEMFIRE/lib/server-dependencies.jar:lib/PreLoader.jar io.pivotal.app.PreLoader
--- Rebalance/src/main/java/io/pivotal/app/PreLoader.java
+++ Rebalance/src/main/java/io/pivotal/app/PreLoader.java
@@ -16,6 +16,7 @@ public class PreLoader {
 
 		// Get the customer region
 		Region<Integer, Customer> customers = cache.getRegion("Customer");
+		//System.out.println("Got the Customer Region: " + customers);
 
 		// Load 20 customers into region
 		CustomerLoader customerLoader = new CustomerLoader(customers);
